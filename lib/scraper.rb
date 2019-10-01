9f703ef4403427d3dad58b9b9b05847bcefa560a
Kierra M <k.merriman32@hotmail.com>
2019-10-01 18:41:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,10 +2,12 @@ require 'nokogiri'
 require 'open-uri'
  
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN")
+
+
+# How to grab HTML Element and text start
 #puts doc.css(".headline-26OIBN")
 # will print HTML elements so element and text we grabbed return nil
 
 #doc.css(".headline-26OIBN").text
 #will return only the text contained inside of element.
-
+#end
