6f689fb8787e6a084671d099c9c12c1134659799
Kierra M <k.merriman32@hotmail.com>
2019-10-01 18:39:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+ 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc.css(".headline-26OIBN")
+#puts doc.css(".headline-26OIBN")
+# will print HTML elements so element and text we grabbed return nil
 
-html = open("https://flatironschool.com/")
+#doc.css(".headline-26OIBN").text
+#will return only the text contained inside of element.
+
